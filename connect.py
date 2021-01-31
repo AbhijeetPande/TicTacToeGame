0b36216fdcbd8f0f92157e884e2093f3a1596562
Abhijeet Pande <abhijeet.cbse@gmail.com>
2021-01-30 22:30:29 -0700

    Circles appear where called

--- connect.py
+++ connect.py
@@ -1,14 +1,16 @@
 from tkinter import *
 from time import *
 
-def onObjectClick(event):                  
-    print('Got object click', event.x, event.y)
-    print(event.widget.find_closest(event.x, event.y))
-
-
 class MyFrame(Frame):
   def __init__(self):
+    self._msg = -1
     Frame.__init__(self)
+    def onObjectClick(event):                  
+      print('Got object click', event.x, event.y)
+      print(event.widget.find_closest(event.x, event.y))
+      self._msg = int(''.join(map(str, event.widget.find_closest(event.x, event.y)))) - 6
+      print(self._msg)
+      print(type(self._msg))
 
     self.myCanvas = Canvas(width=300, height=300, bg="lightblue")
     self.myCanvas.grid()
@@ -21,6 +23,8 @@ class MyFrame(Frame):
 
     my_line_id_3 = self.myCanvas.create_line(90, 130 , 210, 130, fill = "black")
     my_line_id_4 = self.myCanvas.create_line(90, 170 , 210, 170, fill = "black")    
+
+    #1 circle
 
     #9 squares - light up if mouse hovers over
     #buttonEnter1 = Button( width=38, height=38).place(x=91, y=91) 
@@ -36,21 +40,22 @@ class MyFrame(Frame):
     lower_square_2 = self.myCanvas.create_rectangle(131, 171, 169, 209, fill = "yellow3")
     lower_square_3 = self.myCanvas.create_rectangle(171, 171, 209, 209, fill = "yellow3")
 
-    #9 crosses
-    #9 circles
-    # my_rect_id = self.myCanvas.create_rectangle(100, 100, 200, 200, fill = "yellow3")
-    # my_s_rect_id = self.myCanvas.create_rectangle(120, 120, 220, 220, fill = "yellow")
-    # my_line_id_o = self.myCanvas.create_line(120, 120 , 200, 120, fill = "yellow")
-    # my_line_id_s = self.myCanvas.create_line(120, 120, 120, 200, fill = "yellow")
-    # my_line_id_t = self.myCanvas.create_line(100, 100, 120, 120, fill = "yellow")
-    # my_edge_1 = self.myCanvas.create_line(100, 200, 120, 200, fill = "yellow3")
-    # my_edge_2 = self.myCanvas.create_line(120, 200, 120, 220, fill = "yellow")
-    # my_edge_3 = self.myCanvas.create_line(200, 100, 200, 120, fill = "yellow3")
-    # my_edge_4 = self.myCanvas.create_line(200, 120, 220, 120, fill = "yellow")
 
-    # My_eye_1 = self.myCanvas.create_oval(130, 130, 150, 150, fill = "white")
-    # My_eye_2 = self.myCanvas.create_oval(190, 130, 210, 150, fill = "white")
+    
+    click_1 = [100, 100, 120, 120]
+    click_2 = [140, 100, 160, 120]
+    click_3 = [180, 100, 200, 120]
 
+    click_4 = [100, 140, 120, 160]
+    click_5 = [140, 140, 160, 160]
+    click_6 = [180, 140, 200, 160]
+    
+    click_7 = [100, 180, 120, 200]
+    click_8 = [140, 180, 160, 200]
+    click_9 = [180, 180, 200, 200]
+
+    My_circle = self.myCanvas.create_oval(0, 0, 0, 0, width = 10)
+    click_container = [click_1, click_2, click_3, click_4, click_5, click_6, click_7, click_8, click_9]
     My_iris_1 = self.myCanvas.create_oval(135, 135, 145, 145, fill = "blue")
     My_iris_2 = self.myCanvas.create_oval(195, 135, 205, 145, fill = "blue")
 
@@ -65,8 +70,15 @@ class MyFrame(Frame):
     self.myCanvas.tag_bind(lower_square_1, '<ButtonPress-1>', onObjectClick)
     self.myCanvas.tag_bind(lower_square_2, '<ButtonPress-1>', onObjectClick)
     self.myCanvas.tag_bind(lower_square_3, '<ButtonPress-1>', onObjectClick)
-
+    
     while True:
+      if self._msg != -1:
+        print("I reached here")
+        print(type(self._msg))
+        print(self._msg)
+        self.myCanvas.coords(My_circle, click_container[self._msg][0], click_container[self._msg][1], click_container[self._msg][2],click_container[self._msg][3])
+        self.myCanvas.update()
+      
       self.myCanvas.coords(My_iris_1, 137, 137, 142, 142)
       self.myCanvas.coords(My_iris_2, 197, 137, 202, 142)
       sleep(1)
@@ -75,8 +87,6 @@ class MyFrame(Frame):
       self.myCanvas.coords(My_iris_2, 195, 135, 205, 145)
       sleep(1)
       self.myCanvas.update()
-
-
 
 frame02 = MyFrame()
 
